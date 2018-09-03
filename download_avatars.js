337b49cf5b62dc05cd6ea13dd57746b0877cab8d
C-Shi <cshi@ualberta.ca>
2018-09-03 15:18:49 -0600

    able to download to certain dir

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,17 @@
+/*
+  1. script avatar url - getRepoContributors
+  2. push into an array
+  3. create a downloadImageByURL function which download image
+  4. loop through the array and download each avatar
+*/
+
 const request = require('request');
 const token = require('./secret');
+const fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader! \n');
 
+// script all avatar url
 function getRepoContributors(repoOwner, repoName, cb) {
   // define a url with repoOwner and repoName passed in
   const options = {
@@ -17,14 +26,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request.get(options, (err, response, body) => {
     if (err) throw err;
     let info = JSON.parse(body)
-    cb(err, info);
+    contributors = cb(err, info);
   })
 
 }
 
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', (err) => {
+      if (err) throw err;
+    })
+    .pipe(fs.createWriteStream('./avatars/avatar.jpg'))
+}
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
+  let avatarList = [];
   result.forEach((person) => {
-    console.log(person.avatar_url)
+    avatarList.push(person.avatar_url);
   })
+
+  // create a directory - now ready to download
+  let dir = './avatars';
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  };
+
+  avatarList.forEach((avatar) => {
+    downloadImageByURL(avatar, dir)
+  })
+
 });
+
