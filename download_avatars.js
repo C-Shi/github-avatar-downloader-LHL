8a64c1485ab0a04436161c2bfa58652468dafd0e
C-Shi <cshi@ualberta.ca>
2018-09-03 15:41:51 -0600

    add comment for function

--- download_avatars.js
+++ download_avatars.js
@@ -9,7 +9,10 @@ const request = require('request');
 const token = require('./secret');
 const fs = require('fs');
 
+// Welcome info for user
 console.log('Welcome to the GitHub Avatar Downloader! \n');
+
+// ***************************************** define function ******************************************
 
 // script all avatar url
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -20,17 +23,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
     'User-Agent': 'C-Shi',
     'Authorization': 'token ' + token.GITHUB_TOKEN
     }
-  }
+  };
 
   // send http request through request module
   request.get(options, (err, response, body) => {
     if (err) throw err;
-    let info = JSON.parse(body)
+    let info = JSON.parse(body); // parse json body into js object
+
+    // when complete, passing the obtained contributors object into callback for further process
     cb(err, info);
-  })
+  });
 
 }
 
+// this function will download the avatar picture
+// this function will be execute by getRepoContributors' callback once all info has been retained
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on('error', (err) => {
@@ -42,9 +49,16 @@ function downloadImageByURL(url, filePath) {
     })
 }
 
+// ******************************** File execution start here *********************************
+
+// Execute the function
+// 1. getRepoContributors executeed, request JSON from the jquery
 getRepoContributors("jquery", "jquery", function(err, result) {
+
+  // when all contributors info has been retained and parsed, callback is executeed!!
   console.log("Errors:", err);
 
+  // this loop will put username/avatar url of contributors into an array for further process
   let avatarList = [];
   result.forEach((person) => {
     let user = [person.login, person.avatar_url];
@@ -57,6 +71,7 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     fs.mkdirSync(dir);
   };
 
+  // for each of the contributor info, execute downloadImageByURL to download the picture
   avatarList.forEach((avatar) => {
     let path = dir + '/' + avatar[0] + '.jpg';
     downloadImageByURL(avatar[1], path)
