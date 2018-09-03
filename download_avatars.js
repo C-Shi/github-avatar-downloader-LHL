b744dc20a866984f7b968e59b8d926e7999fe421
C-Shi <cshi@ualberta.ca>
2018-09-03 14:08:54 -0600

    define url path

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,15 @@
 const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // define a url with repoOwner and repoName passed in
+  const url = `https://github.com/repos/${repoOwner}/${repoName}/stats/contributors`;
+
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
